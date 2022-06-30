d598f22c148790964ce6222cf71ff89c45c7180d
SuHyun Kyung <ase3238@gmail.com>
2022-06-30 20:14:19 +0900

    solve problem 20300

---
+++ D220630/P20300/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("D220630/P20300/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        PriorityQueue<Long> spq = new PriorityQueue<>(Comparator.naturalOrder());
+        PriorityQueue<Long> lpq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            long curr = Long.parseLong(st.nextToken());
+            spq.add(curr);
+            lpq.add(curr);
+        }
+
+        long max = 0;
+        if (N % 2 == 1) {
+            max = lpq.poll();
+        }
+        for (int i = 0; i < N/2; i++) {
+            long curr = spq.poll() + lpq.poll();
+            max = Math.max(max, curr);
+        }
+        System.out.println(max);
+    }
+}
