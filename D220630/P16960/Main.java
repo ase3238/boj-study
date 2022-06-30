43e1c19aa34c8317e1fd4f11371ae17e21d8b9a5
SuHyun Kyung <ase3238@gmail.com>
2022-06-30 16:01:19 +0900

    solve problem 16960

---
+++ D220630/P16960/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("D220630/P16960/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] lampCount = new int[M + 1];
+        List<Integer>[] lampSwitch = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            lampSwitch[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int count = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < count; j++) {
+                int idx = Integer.parseInt(st.nextToken());
+                lampSwitch[i].add(idx);
+                lampCount[idx]++;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            boolean flag = false;
+            for(int idx: lampSwitch[i]) {
+                if (lampCount[idx] == 1) {
+                    flag = true;
+                }
+                if (flag) {
+                    break;
+                }
+            }
+            if (flag) {
+                continue;
+            } else {
+                System.out.println(1);
+                return;
+            }
+        }
+        System.out.println(0);
+        return;
+    }
+}
