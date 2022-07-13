2584ce0a8e5ce629bb1a2222c7fa3e01fd09ef53
SuHyun Kyung <ase3238@gmail.com>
2022-07-13 12:43:25 +0900

    add comment

--- D220713/P2512/Main.java
+++ D220713/P2512/Main.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 예산
+ * https://www.acmicpc.net/problem/2512
+ * Parametric Search
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("D220713/P2512/input.txt"));
