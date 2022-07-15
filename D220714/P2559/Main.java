1fee43a83658a196b0bb3333f31be9c43c688d08
SuHyun Kyung <ase3238@gmail.com>
2022-07-15 13:46:13 +0900

    solve problem 2559

---
+++ D220714/P2559/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 수열
+ * https://www.acmicpc.net/problem/2559
+ * 누적합, 슬라이딩 윈도우
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("D220714/P2559/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+            if (i < K) {
+                sum += numbers[i];
+            }
+        }
+        
+        int max = sum;
+        for (int i = K; i < N; i++) {
+            sum = sum + numbers[i] - numbers[i - K];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+}
