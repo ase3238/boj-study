05ff1c5a993a375d8260e42fba7e54c91cb2f576
SuHyun Kyung <ase3238@gmail.com>
2022-08-05 12:00:57 +0900

    add comment

--- DS/P1935/Main.java
+++ DS/P1935/Main.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+/*
+ * 후위 표기식
+ * https://www.acmicpc.net/problem/1935
+ * 구현, Stack
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("DS/P1935/input.txt"));
